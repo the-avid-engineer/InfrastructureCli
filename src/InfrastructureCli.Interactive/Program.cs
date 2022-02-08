bab09b39d7e8705d5458c71f8373bcd6e64da4bc
Chris Philips <the.avid.engineer@outlook.com>
2022-02-08 15:13:18 -0800

    feat: add a console app to the sln, so it can be run locally

---
+++ src/InfrastructureCli.Interactive/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using InfrastructureCli.Commands;
+
+namespace InfrastructureCli.Interactive
+{
+    public static class Program
+    {
+        public static Task<int> Main(string[] args)
+        {
+            return new ProgramCommand(Array.Empty<IGenerateCommand>()).Invoke(args);
+        }
+    }
+}
