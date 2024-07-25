ed362c6d1be431b75d20ac126d48ca797065b45c
Chris Philips <chris.philips@traxretail.com>
2024-07-24 21:34:05 -0700

    chore: better variable names

--- src/InfrastructureCli/Commands/NewCommand.cs
+++ src/InfrastructureCli/Commands/NewCommand.cs
@@ -5,16 +5,16 @@ namespace InfrastructureCli.Commands;
 
 internal record NewCommand : CommandBase
 {
-    public static void Attach(RootCommand rootCommand, IEnumerable<IChildCommand> generateCommands)
+    public static void Attach(RootCommand rootCommand, IEnumerable<IChildCommand> childCommands)
     {
         var newCommand = new Command("new")
         {
             Description = "Generate the files needed in order to execute other commands."
         };
 
-        foreach (var generateCommand in generateCommands)
+        foreach (var command in childCommands)
         {
-            generateCommand.Attach(newCommand);
+            command.Attach(newCommand);
         }
 
         rootCommand.AddCommand(newCommand);
